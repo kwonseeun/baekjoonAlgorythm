38f3cc6d593e82d89b9730811b93685ca727b5de
kwonseeun <0115km@naver.com>
2022-03-08 14:53:04 +0900

    add 2739

---
+++ baekjoon/src/baekjoon/baekjoon2739.java
@@ -0,0 +1,16 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class baekjoon2739 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int a = sc.nextInt();
+		
+		for (int i = 0; i < 10; i++) {
+			System.out.println(a +" x " + i + "=" +(a*i));
+		}
+	}
+}
