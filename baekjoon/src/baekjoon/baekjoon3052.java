741f8119ffac8b2cb83c6a56689e18dc8099fda4
kwonseeun <0115km@naver.com>
2022-04-20 11:22:43 +0900

    3052 생성

--- baekjoon/src/baekjoon/baekjoon3052.java
+++ baekjoon/src/baekjoon/baekjoon3052.java
@@ -4,7 +4,7 @@ import java.util.HashSet;
 import java.util.Scanner;
 
 /**두 자연수 a와 b가 있을때, a%b는 a를 b로 나눈 나머지이다. 예를 들어, 7,14,27,38을 3으로 나눈 나머지는 1, 2,0,2이다.
- * 수 10개를 입력 받은뒤, 이를 42로 나눈 나머지를 구한다. 그 다음 서로 다른 값이 몇 개 있는지 출력하는 프로그램을 작성하시*/
+ * 수 10개를 입력 받은뒤, 이를 42로 나눈 나머지를 구한다. 그 다음 서로 다른 값이 몇 개 있는지 출력하는 프로그램을 작성하시오 */
 public class baekjoon3052 {
 
 	public static void main(String[] args) {
