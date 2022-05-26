26b75d9f56afa994d08760e1241b26f6dcdb3ddc
kwonseeun <0115km@naver.com>
2022-05-26 16:55:18 +0900

    백준 문제

--- baekjoon/src/baekjoon/backjoon1330.java
+++ baekjoon/src/baekjoon/backjoon1330.java
@@ -6,16 +6,16 @@ public class backjoon1330 {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		
+
 		int a = sc.nextInt();
 		int b = sc.nextInt();
 		
-		if (a>b)
-			System.out.println("<");
-		else if (a<b)
+		if (a > b ) {
 			System.out.println(">");
-		else 
-			System.out.println("=");
+		}else if (a < b ) {
+			System.out.println("<");
+		} else  if(a == b ) {
+			System.out.println("==");
+		}
 	}
-
 }
---
+++ baekjoon/src/baekjoon/baekjoon9498.java
@@ -0,0 +1,26 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class baekjoon9498 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("점수를 입력해주세요: ");
+		int sum = sc.nextInt();
+		
+		if (sum >= 90 ) {
+			System.out.println("A");
+		} else if (sum >= 80 ) {
+			System.out.println("B");
+		} else if (sum >= 70) {
+			System.out.println("C");	
+		} else if (sum >= 60) {
+			System.out.println("D");
+		} else {
+			System.out.println("F");
+		}
+	}
+
+}
