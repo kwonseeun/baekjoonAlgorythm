6f4286056d2701816552dc63bf9640e6e911be1b
kwonseeun <0115km@naver.com>
2022-07-28 18:11:43 +0900

    8393번 추가

---
+++ baekjoon/src/forExample/b8393.java
@@ -0,0 +1,22 @@
+package forExample;
+
+import java.util.Scanner;
+
+//n이 주어졌을 때, 1부터 n까지 합을 구하는 프로그램을 작성하시오.
+public class b8393 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int a = sc.nextInt();
+		sc.close();
+		int sum = 0; 
+		
+		for (int i = 1; i <= a; i++) {
+			sum += i ;
+		}
+		
+		System.out.println(sum);
+	}
+
+}
