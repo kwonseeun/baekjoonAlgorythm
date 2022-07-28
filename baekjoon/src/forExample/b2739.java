914b9a6f7093e719303d5c9bed743676e300c7ee
kwonseeun <0115km@naver.com>
2022-07-28 18:06:36 +0900

    10950, 2739 add

--- baekjoon/src/forExample/b2739.java
+++ baekjoon/src/forExample/b2739.java
@@ -2,18 +2,15 @@ package forExample;
 
 import java.util.Scanner;
 
-public class baekjoon2739 {
+public class b2739 {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
-		System.out.println("출력할 단을 입력해주세요: ");
 		int a = sc.nextInt();
 		
-		System.out.println(a + "단 출력합니다. " );
-		
 		for (int i = 1; i <= 9; i++) {
-			System.out.println(a + "*" + i + "=" + a * i);
+			System.out.println(a + " * " + i + " = " + a * i);
 		}
 	}
 }
