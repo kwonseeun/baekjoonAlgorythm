e06ce71f299e29fddb3809cced8efe7b6ba7e3cb
kwonseeun <0115km@naver.com>
2022-07-29 18:04:16 +0900

    15552 빠른 A+B

---
+++ baekjoon/src/forExample/b15552.java
@@ -0,0 +1,15 @@
+package forExample;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class b15552 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(br.readLine());
+	}
+
+}
