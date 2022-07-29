e33d0f74ad8489a41f235269c575575cb2511de1
kwonseeun <0115km@naver.com>
2022-07-29 18:07:03 +0900

    15552 코드 수정

--- baekjoon/src/forExample/b15552.java
+++ baekjoon/src/forExample/b15552.java
@@ -3,6 +3,7 @@ package forExample;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class b15552 {
 
@@ -10,6 +11,16 @@ public class b15552 {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
 		int N = Integer.parseInt(br.readLine());
+		
+		StringTokenizer st;
+		
+		for (int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine()," ");
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			
+			System.out.println(a + b);
+		}
 	}
 
 }
