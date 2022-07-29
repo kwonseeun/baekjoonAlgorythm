0be5b33adf54cb9320ea86b061b16141852d244a
kwonseeun <0115km@naver.com>
2022-07-29 18:15:03 +0900

    2471 추가 N찍기

--- baekjoon/src/forExample/b8393.java
+++ baekjoon/src/forExample/b8393.java
@@ -8,11 +8,11 @@ public class b8393 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
-		int a = sc.nextInt();
+		int n = sc.nextInt();
 		sc.close();
 		int sum = 0; 
 		
-		for (int i = 1; i <= a; i++) {
+		for (int i = 1; i < n+1; i++) {
 			sum += i ;
 		}
 		
