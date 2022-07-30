096c3a0be59f326703f6c837b7d9cb86dc426540
kwonseeun <0115km@naver.com>
2022-07-30 23:59:48 +0900

    2742 add

--- baekjoon/src/forExample/b2742.java
+++ baekjoon/src/forExample/b2742.java
@@ -1,10 +1,17 @@
 package forExample;
 
+import java.util.Scanner;
+
 public class b2742 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner sc = new Scanner(System.in);
+		
+		int a = sc.nextInt();
+		
+		for (int i = a; i > 0; i--) {
+			System.out.println(i);
+		}
 	}
 
 }
