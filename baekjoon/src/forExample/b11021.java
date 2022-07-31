813404db3848994543f7c2a448ab663dd3c65439
kwonseeun <0115km@naver.com>
2022-07-31 23:35:14 +0900

    11021 A+B-7

---
+++ baekjoon/src/forExample/b11021.java
@@ -0,0 +1,20 @@
+package forExample;
+
+import java.util.Scanner;
+
+public class b11021 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		
+		int t = 0;
+		
+		for (int i = 0; i < t; i++) {
+			System.out.println(a + b);
+		}
+	}
+
+}
