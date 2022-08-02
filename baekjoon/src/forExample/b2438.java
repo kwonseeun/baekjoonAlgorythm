16246e7013d78143e5f8629146a4d78f86287c76
kwonseeun <0115km@naver.com>
2022-08-02 23:47:23 +0900

    2439 add

--- baekjoon/src/forExample/b2438.java
+++ baekjoon/src/forExample/b2438.java
@@ -14,7 +14,7 @@ public class b2438 {
 			for (int j = 1; j <= i; j++) {
 				System.out.print("*");
 			}
-			System.out.println();
+			System.out.println("");
 		} 
 	}
 }
---
+++ baekjoon/src/forExample/b2439.java
@@ -0,0 +1,10 @@
+package forExample;
+
+public class b2439 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
