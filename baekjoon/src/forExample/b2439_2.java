533037395dd87ee83ef4888a90a3689288cb665f
kwonseeun <0115km@naver.com>
2022-08-03 23:51:57 +0900

    add

--- baekjoon/src/forExample/b2439_2.java
+++ baekjoon/src/forExample/b2439_2.java
@@ -1,10 +1,18 @@
 package forExample;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class b2439_2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+		 BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	        int n = Integer.parseInt(br.readLine());
+	        StringBuilder sb = new StringBuilder();
+	        for (int i = 1; i <= n; i++) {
+	            
+	            sb.append("\n");
+	        }
+	        System.out.print(sb);
+	   }
 }
