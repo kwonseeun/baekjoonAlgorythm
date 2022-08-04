986a78005a33a0f3c00a9a9bb96e369468c473bb
kwonseeun <0115km@naver.com>
2022-08-04 23:57:23 +0900

    for문 추가

--- baekjoon/src/forExample/b11022.java
+++ baekjoon/src/forExample/b11022.java
@@ -1,10 +1,20 @@
 package forExample;
 
+import java.util.Scanner;
+
 public class b11022 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int t = sc.nextInt();
+		
+		for (int i = 0; i < t; i++) {
+			int a = sc.nextInt();
+			int b = sc.nextInt();
+			
+			System.out.println( a + b - 8);
+		}
 	}
-
 }
