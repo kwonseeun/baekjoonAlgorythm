5b8b09cf4d168f2e1f6eb6b85ae6697ad3e3c7fc
kwonseeun <0115km@naver.com>
2022-08-05 00:01:10 +0900

    update

--- baekjoon/src/forExample/b11022.java
+++ baekjoon/src/forExample/b11022.java
@@ -2,6 +2,7 @@ package forExample;
 
 import java.util.Scanner;
 
+// 두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
 public class b11022 {
 
 	public static void main(String[] args) {
@@ -14,7 +15,7 @@ public class b11022 {
 			int a = sc.nextInt();
 			int b = sc.nextInt();
 			
-			System.out.println( a + b - 8);
+			System.out.println("Case #" + i + ": " + a + " + " + b + " = " + (a+b));
 		}
 	}
 }
