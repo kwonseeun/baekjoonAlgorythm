af1e9e3f6e0db37905c21714c7466425006f64dd
kwonseeun <0115km@naver.com>
2022-08-06 23:35:01 +0900

    X보다 작은수

---
+++ baekjoon/src/forExample/b10871.java
@@ -0,0 +1,19 @@
+package forExample;
+
+import java.util.Scanner;
+
+public class b10871 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int s = sc.nextInt();
+		
+		for (int i = 0; i < args.length; i++) {
+			int a = sc.nextInt();
+			int b = sc.nextInt();
+					
+		}
+	}
+
+}
