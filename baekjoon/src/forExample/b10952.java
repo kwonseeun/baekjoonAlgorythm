8fe2121736208d09a1220f87219085e74303a987
kwonseeun <0115km@naver.com>
2022-08-07 13:26:55 +0900

    update

--- baekjoon/src/forExample/b10952.java
+++ baekjoon/src/forExample/b10952.java
@@ -1,10 +1,22 @@
 package forExample;
 
+import java.util.Scanner;
+
 public class b10952 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Scanner sc = new Scanner(System.in);
+		
+		while (true) {
+			
+			int A = sc.nextInt();
+			int B = sc.nextInt();
+			
+			if (A==0 && B==0) {
+				sc.close();
+			}
+		}
 	}
 
 }
