01095fa2a004ea61c24c8bbbd6387cb7f4c5ecdb
kwonseeun <0115km@naver.com>
2022-08-09 00:30:10 +0900

    add

--- baekjoon/src/forExample/b1463.java
+++ baekjoon/src/forExample/b1463.java
@@ -1,10 +1,14 @@
 package forExample;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class b1463 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws IOException{
 
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	}
 
 }
