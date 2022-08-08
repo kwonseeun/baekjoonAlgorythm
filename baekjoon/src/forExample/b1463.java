dccdfe029f3274d2060ac19093fbc0e4dd0429a7
kwonseeun <0115km@naver.com>
2022-08-09 00:31:31 +0900

    update

--- baekjoon/src/forExample/b1463.java
+++ baekjoon/src/forExample/b1463.java
@@ -9,6 +9,13 @@ public class b1463 {
 	public static void main(String[] args) throws IOException{
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		
+		int[] arr = new int[N + 1];
+		arr[0] = 0;
+		arr[1] = 1;
+		
+		
 	}
 
 }
