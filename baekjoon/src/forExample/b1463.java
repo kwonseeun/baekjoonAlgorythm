98e446ae121bbf9dfa2e2574ead65e4421386368
kwonseeun <0115km@naver.com>
2022-08-10 01:55:47 +0900

    update

--- baekjoon/src/forExample/b1463.java
+++ baekjoon/src/forExample/b1463.java
@@ -1,7 +1,8 @@
 package forExample;
 
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.util.Scanner;
+import java.io.InputStreamReader;
 
 public class b1463 {
 
@@ -19,7 +20,20 @@ public class b1463 {
 
 		    private static int dp(int n) {
 		        if (n == 1)
-		       
+		            return 0;
+		        if (d[n] > 0)
+		            return d[n];
+		        d[n] = dp(n - 1) + 1;
+		        if (n % 2 == 0) {
+		            int tmp = dp(n / 2) + 1;
+		            if (d[n] > tmp)
+		                d[n] = tmp;
+		        }
+		        if (n % 3 == 0) {
+		            int tmp = dp(n / 3) + 1;
+		            if (d[n] > tmp)
+		                d[n] = tmp;
+		        }
 		        return d[n];
 	}
 
