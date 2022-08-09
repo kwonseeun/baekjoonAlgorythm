367b0dcdc1d6fc0fd88baa683b81074f4929dba2
kwonseeun <0115km@naver.com>
2022-08-09 22:27:15 +0900

    update

--- baekjoon/src/forExample/b1463.java
+++ baekjoon/src/forExample/b1463.java
@@ -1,21 +1,22 @@
 package forExample;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 
 public class b1463 {
 
 	public static void main(String[] args) throws IOException{
+		  static int[] d;
 
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int N = Integer.parseInt(br.readLine());
-		
-		int[] arr = new int[N + 1];
-		arr[0] = 0;
-		arr[1] = 1;
-		
-		
+		    public static void main(String[] args) {
+		        Scanner sc = new Scanner(System.in);
+		        int n = sc.nextInt();
+		        d = new int[n + 1];
+
+		        System.out.println(dp(n));
+		        sc.close();
+		    }
+
+		        return d[n];
 	}
 
 }
