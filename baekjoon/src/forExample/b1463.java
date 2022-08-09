719fbff0e9d5e86c26f834bd98c732154c28628a
kwonseeun <0115km@naver.com>
2022-08-10 01:56:26 +0900

    update

--- baekjoon/src/forExample/b1463.java
+++ baekjoon/src/forExample/b1463.java
@@ -3,10 +3,10 @@ package forExample;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Scanner;
 
 public class b1463 {
 
-	public static void main(String[] args) throws IOException{
 		  static int[] d;
 
 		    public static void main(String[] args) {
