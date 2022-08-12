5aff0b8d31e59773e6e94a7fd3ca2fb5c2c33a37
kwonseeun <0115km@naver.com>
2022-08-12 21:54:42 +0900

    re add

--- baekjoon/src/forExample/b2023.java
+++ baekjoon/src/forExample/b2023.java
@@ -1,9 +1,34 @@
 package forExample;
 
+import java.util.Scanner;
+
 public class b2023 {
-
+	static int n;
+	
 	public static void main(String[] args) {
-
+		Scanner sc = new Scanner(System.in);
+    	n = sc.nextInt();
+    	re(1,2);
+    	re(1,3);
+    	re(1,5);
+    	re(1,7);
+	}
+	
+	static void re (int cnt, int num) {
+		if (cnt == n) {
+			if(isprime(num))
+				System.out.println(num);
+			return;
+		}
+		for (int i = 1; i <10; i++) {
+			if (i == 2 || i == 4 || i == 6 || i == 8) {
+				if (isprime(num* 1 0 + 1)) {
+					re(cnt+1, num*10+i);
+				}
+			}
+			
+		
+		}
 	}
 
 }
