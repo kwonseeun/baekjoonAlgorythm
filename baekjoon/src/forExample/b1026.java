4e85f539fe8a589e50fcadc895611c583e77084c
kwonseeun <0115km@naver.com>
2022-08-14 23:07:01 +0900

    update

--- baekjoon/src/forExample/b1026.java
+++ baekjoon/src/forExample/b1026.java
@@ -6,12 +6,14 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
-/* 옛날 옛적에 수학이 항상 큰 골치거리였던 나라가 있었다. 이 나라의 국왕 김지민은 다음과 
+/*
+ *  옛날 옛적에 수학이 항상 큰 골치거리였던 나라가 있었다. 이 나라의 국왕 김지민은 다음과 
  같은 문제를 내고 큰 상금을 걸었다. 
  길이가 N인 정수 배열 A와 B가 있다. 다음과 같이 함수 S를 정의하자. 
  S = A[0] x B[0] + ... + A[N-1] x B[N-1]
  S의 값을 가장 작게 만들기 위해 A의 수를 재배열하자. 단, B에 있는 수는 재배열하면 안된다. 
  S의 최솟값을 출력하는 프로그램을 작성하시오.
+ 
  */ 
 
 public class b1026 {
@@ -41,6 +43,11 @@ public class b1026 {
 		Arrays.sort(arrA);
 		Arrays.sort(arrB);
 		
+		for (int i = 0; i < n; i++) {
+			sum += (arrA[i] * arrB[n-i-1]);
+			
+			System.out.println(sum);
+		}
 	}
 
 }
