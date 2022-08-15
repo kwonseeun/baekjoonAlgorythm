f7abbf04931b8bca6b976d847e8578ea2e481349
kwonseeun <0115km@naver.com>
2022-08-15 23:28:40 +0900

    kbs1부터 첫번쨰로

--- baekjoon/src/forExample/b2816.java
+++ baekjoon/src/forExample/b2816.java
@@ -11,6 +11,20 @@ public class b2816 {
 	        for(int i = 0 ; i<n; i++) {
 	        	arr[i] = sc.next();
 	        }
+	    	for(int i = 0 ;i<n; i++) {
+	       		if(arr[i].equals("KBS1")) {//KBS1부터 첫번째로
+	       			String tmp = "";
+	       			for(int j = i; j>0; j--) {
+	        			System.out.print(4);
+	        			tmp = arr[j-1];
+	        			arr[j-1] = arr[j];
+	        			arr[j] = tmp;
+	       			}
+	       			break;
+	        	}
+	        	else 
+	        		System.out.print(1);
+	       	}
 	}
 
 }
