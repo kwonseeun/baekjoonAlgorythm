536521a6e6524278ddc32be4fd0baca77a19ad64
kwonseeun <0115km@naver.com>
2022-10-21 23:38:33 +0900

    update

--- baekjoon/src/baekjoon09/B_최소_최대.java
+++ baekjoon/src/baekjoon09/B_최소_최대.java
@@ -11,5 +11,21 @@ public class B_최소_최대 {
 		for (int i = 0; i < N; i++) {
 			arr[i] = sc.nextInt();
 		}
+		
+		// min 
+		int min = arr[0];
+		
+		for (int i = 0; i < arr.length; i++) {
+			if(arr[i] < min ) min = arr[i];
+		}
+		
+		// max
+		int max = arr[0];
+		
+		for (int i = 0; i < arr.length; i++) {
+			if(arr[i] > max) max = arr[i];
+		}
+		
+		
 	}
 }
