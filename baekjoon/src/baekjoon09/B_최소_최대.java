efe1d07e34b46c19ebf10b7bf4fb8e9f45201132
kwonseeun <0115km@naver.com>
2022-10-21 23:36:49 +0900

    create

---
+++ baekjoon/src/baekjoon09/B_최소_최대.java
@@ -0,0 +1,15 @@
+package baekjoon09;
+
+import java.util.Scanner;
+
+public class B_최소_최대 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int[] arr = new int[N];
+		
+		for (int i = 0; i < N; i++) {
+			arr[i] = sc.nextInt();
+		}
+	}
+}
