dc636dc79a10ea40ac8fa276a9a3c5a51db37a35
kwonseeun <0115km@naver.com>
2022-10-24 22:37:25 +0900

    선택정렬

---
+++ baekjoon/src/TEST/ArrayTest.java
@@ -0,0 +1,22 @@
+package TEST;
+
+import java.util.Arrays;
+
+public class ArrayTest {
+	 public static void main(String[] args) {
+	        
+	        int []arr = {7, 5, 9, 0, 3, 1, 6, 2, 4, 8};
+	        
+	        int minIndex = 0; //가장 적은 원소의 인덱스
+	        
+	        for(int i=0; i < arr.length; i++){
+	            for(int j=i+1 ; j < arr.length; j++){
+	                if(arr[minIndex] > arr[j])
+	                    minIndex = j;
+	            }
+	            
+	
+	        }
+	        System.out.println(Arrays.toString(arr));
+	    }
+}
