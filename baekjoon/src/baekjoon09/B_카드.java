d21ed2fceb062880aeac494a692f8445ff2897de
kwonseeun <0115km@naver.com>
2022-10-28 23:17:05 +0900

    update

--- baekjoon/src/baekjoon09/B_카드.java
+++ baekjoon/src/baekjoon09/B_카드.java
@@ -1,5 +1,25 @@
 package baekjoon09;
 
-public class B_카드 {
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
 
+public class B_카드 {
+	 public static void main(String[] args) {
+	        Scanner sc = new Scanner(System.in);
+	        int n = sc.nextInt();
+	        int last = 1;
+	        Queue<Integer> q = new LinkedList<>();
+
+	        for(int i = 1; i <= n; i++) {	//큐에 카드 넣기
+	            q.offer(i);
+	        }
+	        
+	        while(!(q.size() == 1)) {
+	            q.poll();	//제일 위 카드 버림
+	            last = q.poll();	//제일 위 카드 last에 저장
+	            q.offer(last);	//last를 마지막으로 옮김
+	        }
+	        System.out.println(last);
+	    }
 }
