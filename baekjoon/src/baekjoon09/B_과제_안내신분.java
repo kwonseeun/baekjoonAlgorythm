5419c9edc4b2045a5f5a5b25ff71b2a6e527a02b
kwonseeun <0115km@naver.com>
2022-10-30 22:05:52 +0900

    create

---
+++ baekjoon/src/baekjoon09/B_과제_안내신분.java
@@ -0,0 +1,12 @@
+package baekjoon09;
+
+import java.util.Scanner;
+
+public class B_과제_안내신분 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int[] arr = new int[30];
+		
+		
+	}
+}
