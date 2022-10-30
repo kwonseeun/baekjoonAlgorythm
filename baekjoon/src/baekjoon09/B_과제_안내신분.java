959d424c177d8418a6dedb59c4dcd45658b3ea3a
kwonseeun <0115km@naver.com>
2022-10-30 22:10:48 +0900

    update

--- baekjoon/src/baekjoon09/B_과제_안내신분.java
+++ baekjoon/src/baekjoon09/B_과제_안내신분.java
@@ -5,8 +5,18 @@ import java.util.Scanner;
 public class B_과제_안내신분 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int[] arr = new int[30];
+		int[] students = new int[31];
 		
-		
+		// 온 사람은 출석체크 
+		for (int i = 0; i <= 28; i++) {
+			int num= sc.nextInt();
+			students[sc.nextInt()] ++;
+			
+			for (int j = 0; j < students.length; j++) {
+				if(students[i] == 0)
+					System.out.println(i);
+			}
+			sc.close();
+		}
 	}
 }
