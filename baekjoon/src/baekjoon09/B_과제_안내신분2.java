a4498d3147f7275b9a576348f07319e3e36122f7
kwonseeun <0115km@naver.com>
2022-10-30 22:12:43 +0900

    과제 안내신분_2

---
+++ baekjoon/src/baekjoon09/B_과제_안내신분2.java
@@ -0,0 +1,21 @@
+package baekjoon09;
+
+import java.util.Scanner;
+
+public class B_과제_안내신분2 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int[] student = new int[31];
+
+		for (int i = 1; i < 29; i++) {
+			int success = sc.nextInt();
+			student[success] = 1;
+		}
+		for (int i = 1; i < student.length; i++) {
+			if (student[i] != 1)
+				System.out.println(i);
+		}
+
+		sc.close();
+	}
+}
