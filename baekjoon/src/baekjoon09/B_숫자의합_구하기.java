853175523533e3854c176083054df5c329063c6e
kwonseeun <0115km@naver.com>
2022-10-31 18:20:11 +0900

    update

--- baekjoon/src/baekjoon09/B_숫자의합_구하기.java
+++ baekjoon/src/baekjoon09/B_숫자의합_구하기.java
@@ -1,5 +1,12 @@
 package baekjoon09;
 
-public class B_숫자의합_구하기 {
+import java.util.Scanner;
 
+public class B_숫자의합_구하기 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+
+		
+	}
 }
