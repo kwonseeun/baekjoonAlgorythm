469e0cea34a92740f397c476db4e5eccd2dd41bd
kwonseeun <0115km@naver.com>
2022-10-31 18:37:18 +0900

    주석 추가

--- baekjoon/src/baekjoon09/B_숫자의합_구하기.java
+++ baekjoon/src/baekjoon09/B_숫자의합_구하기.java
@@ -14,5 +14,12 @@ public class B_숫자의합_구하기 {
 		for (int i = 0; i < cNum.length; i++) {
 			sum+= cNum[i] - '0'; // cNum에 있는 char값을 저장 후 아스키코드로 변환하고, int형으로 바꿔줄 때는 문자열로 마이너스를 해준다. 
 		}
+		System.out.println(sum);
 	}
 }
+/* N값 입력받기 
+ * 길이 N의 숫자를 입력받아 String형 변수에 sNum을 저장하기
+ * int형 변수 sum 선언하기 
+ * for(cNum 길이만큼 반복하기) {
+ * 배열의 각 자릿값을 정수형으로 변환하여 sum에 누적하기 
+ * }*/
