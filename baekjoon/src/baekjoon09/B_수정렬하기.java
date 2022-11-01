dd447b4daacb4d6845bce2722e16b91b2352fc35
kwonseeun <0115km@naver.com>
2022-11-01 23:35:30 +0900

    update

---
+++ baekjoon/src/baekjoon09/B_수정렬하기.java
@@ -0,0 +1,27 @@
+package baekjoon09;
+
+import java.util.Scanner;
+
+public class B_수정렬하기 {
+	Scanner sc = new Scanner(System.in);
+
+	int n = sc.nextInt();
+	int[] a = new int[n];
+	for (int i = 0; i < n; i++) {
+		a[i] = sc.nextInt();
+	}
+	for (int i = 0; i < n; i++) {
+		for (int j = i + 1; j < n; j++) {
+			if (a[i] > a[j]) {
+				int tmp = a[i];
+				a[i] = a[j];
+				a[j] = tmp;
+
+			}
+		}
+	}
+	for (int i = 0; i < n; i++) {
+		System.out.println(a[i]);
+	}
+}
+}
