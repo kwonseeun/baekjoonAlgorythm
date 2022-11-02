e19cba1e4f245c1da59fc384b19f083c1ba2e7c1
kwonseeun <0115km@naver.com>
2022-11-02 22:53:30 +0900

    update

--- baekjoon/src/baekjoon09/B_구간_합구하기.java
+++ baekjoon/src/baekjoon09/B_구간_합구하기.java
@@ -1,5 +1,15 @@
 package baekjoon09;
 
-public class B_구간_합구하기 {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
+public class B_구간_합구하기 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+	}
 }
