6e7f2c0a92e6d0b8ce12ef96543ac2e795878c57
kwonseeun <0115km@naver.com>
2022-11-02 22:57:46 +0900

    update

--- baekjoon/src/baekjoon09/B_구간_합구하기.java
+++ baekjoon/src/baekjoon09/B_구간_합구하기.java
@@ -11,5 +11,15 @@ public class B_구간_합구하기 {
 		
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		
+		int suNo = Integer.parseInt(st.nextToken());
+		int quizNo = Integer.parseInt(st.nextToken());
+		
+		long[] S = new long[suNo + 1]; // 합배열 선언, Int로 선언하면 값이 넘어가기 때문에 long형을 써준다. +1 해준 이유는,배열은 0번째 인덱스부터 시작하기 때문에 0을 무시하려 +1로 함
+		
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i <= suNo; i++) {
+			S[i] = S[i + 1] + Integer.parseInt(st.nextToken());
+		}
+				
 	}
 }
