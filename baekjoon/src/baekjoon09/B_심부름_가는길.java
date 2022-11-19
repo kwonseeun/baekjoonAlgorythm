33d9581b7a81b2a77a6c734b4ceec69cdcee7068
kwonseeun <0115km@naver.com>
2022-11-19 23:54:45 +0900

    update

--- baekjoon/src/baekjoon09/B_심부름_가는길.java
+++ baekjoon/src/baekjoon09/B_심부름_가는길.java
@@ -1,5 +1,20 @@
 package baekjoon09;
 
-public class B_심부름_가는길 {
+import java.util.Scanner;
 
+public class B_심부름_가는길 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int result = 0;
+		
+		for (int i = 0; i < 4; i++) {
+			result += sc.nextInt();
+		}
+		
+		int min = result / 60;
+		int sec = result % 60;
+		
+		System.out.println(min);
+		System.out.println(sec);
+	}
 }
