b3afa9e780f5ce164c3f89796bcae61343c890e9
kwonseeun <0115km@naver.com>
2022-11-21 18:36:04 +0900

    update

---
+++ baekjoon/src/baekjoon09/B_주사위세개.java
@@ -0,0 +1,35 @@
+package baekjoon09;
+
+import java.util.Scanner;
+
+public class B_주사위세개 {
+	public static void main( String[] args ) {
+	    
+    	Scanner sc = new Scanner( System.in );
+    
+    	int a = sc.nextInt();
+    	int b = sc.nextInt();
+    	int c = sc.nextInt();
+    
+    	sc.close();
+    
+    	if( a==b && a==c ) {
+        	System.out.println( 10000 + a*1000 );
+        } else if( a==b && a!=c ) {
+        	System.out.println( 1000 + a*100 );
+        } else if( a==c && a!=b ) {
+        	System.out.println( 1000 + a*100 );
+        } else if( b==c && a!=b ) {
+        	System.out.println( 1000 + b*100 );
+        } else {
+        	int max = a;
+            if( max < b ) {
+            	max = b;
+            }
+            if( max < c ) {
+            	max = c;
+            }
+            System.out.println( max*100);
+        }
+    }
+}
