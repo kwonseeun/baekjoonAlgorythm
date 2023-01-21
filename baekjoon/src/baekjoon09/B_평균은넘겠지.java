d9c2fbe047676a9f149b144607f8b0013d69b8f7
kwonseeun <0115km@naver.com>
2023-01-21 23:34:37 +0900

    update

---
+++ baekjoon/src/baekjoon09/B_평균은넘겠지.java
@@ -0,0 +1,25 @@
+package baekjoon09;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class B_평균은넘겠지 {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		int c = Integer.parseInt(br.readLine()), n, sum, cnt;
+		String[] tmp;
+		while(c-- > 0) {
+			sum = cnt = 0;
+			tmp = br.readLine().split(" ");
+			n = Integer.parseInt(tmp[0]);
+			
+			}
+		bw.flush();
+		bw.close();
+	}
+}
