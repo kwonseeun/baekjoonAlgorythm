4cb43fe2ccd586b6e3eedd8050721cdc98a94d74
kwonseeun <0115km@naver.com>
2023-01-26 23:54:56 +0900

    백준 ISBN

---
+++ baekjoon/src/baekjoon08/B_ISBN.java
@@ -0,0 +1,27 @@
+package baekjoon08;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class B_ISBN {
+    private static String ISBN = "9780921418";
+    private static int mod13 = 3;
+
+    public static void main(String[] args) throws IOException {
+        final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int sum = 0;
+        for (String eachDigit : ISBN.split("")) {
+            sum += Integer.parseInt(eachDigit) * getMod123();
+        }
+
+        for (int i = 0; i < 3; i++) {
+            sum += Integer.parseInt(br.readLine()) * getMod123();
+        }
+
+        System.out.println("The 1-3-sum is " + sum);
+    }
+
+  
+}
