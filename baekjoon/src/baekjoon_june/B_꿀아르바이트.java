466c1aca786c2f51eed3c5360f5cece051055ffc
kwonseeun <0115km@naver.com>
2023-05-21 22:56:14 +0900

    백준_12847_꿀아르바이트

---
+++ baekjoon/src/baekjoon_june/B_꿀아르바이트.java
@@ -0,0 +1,29 @@
+package baekjoon_june;
+
+import java.util.Scanner;
+
+public class B_꿀아르바이트 {
+	 public static void main(String[] args) {
+	        Scanner sc = new Scanner(System.in);
+	        int n = sc.nextInt();
+	        int m = sc.nextInt();
+	        long answer=0;
+	        long sum = 0;
+
+	        int[] arr = new int[n];
+	        for(int i=0; i<n; i++){
+	            arr[i]=sc.nextInt();
+	        }
+
+	        for(int i=0; i<m; i++){
+	            sum +=arr[i];
+	        }
+	        answer = sum;
+
+	        for(int i=m; i<n; i++){
+	            sum += (arr[i]-arr[i-m]);
+	            answer = Math.max(answer, sum);
+	        }
+	        System.out.println(answer);
+	    }
+}
