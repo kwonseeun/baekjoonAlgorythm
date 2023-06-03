8b73b2c1ff4e6ca1e94e73af69392f3e902ee766
kwonseeun <0115km@naver.com>
2023-06-03 23:49:36 +0900

    백준_별찍기

---
+++ baekjoon/src/baekjoon_june/B_별찍기.java
@@ -0,0 +1,22 @@
+package baekjoon_june;
+
+import java.util.Scanner;
+
+public class B_별찍기 {
+	 Scanner sc = new Scanner(System.in);
+     int n = sc.nextInt();
+
+     for (int i = 0; i <= n; i++) {
+         int l = 0;
+         for (int j = 0; j < i; j++) {
+             System.out.print(" ");
+             l++;
+         }
+
+         for (int k = l + 1; k <= n; k++) {
+             System.out.print("*");
+         }
+         System.out.println();
+     	}
+ 	}
+}
