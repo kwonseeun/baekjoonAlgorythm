8c6846beb6f699c2607b765d98b870b424bc3f03
kwonseeun <0115km@naver.com>
2023-06-08 23:35:56 +0900

    백준_X보다작은수

---
+++ baekjoon/src/baekjoon_june/B_X보다_작은수.java
@@ -0,0 +1,19 @@
+package baekjoon_june;
+
+import java.util.Scanner;
+
+public class B_X보다_작은수 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int X = sc.nextInt();
+		
+		int[] A = new int[N];
+		for(int i=0; i<A.length; i++) {
+			A[i] = sc.nextInt();
+			if(A[i] < X) {
+				System.out.printf("%d ", A[i]);
+			}
+		}
+	}
+}
