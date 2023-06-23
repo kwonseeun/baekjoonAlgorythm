5ff65ab7547e9009ea3eda587fb825e76092c4e8
kwonseeun <0115km@naver.com>
2023-06-23 23:52:11 +0900

    정수 내림차순으로 배치

---
+++ baekjoon/src/forExample/P_정수내림차순으로_배치.java
@@ -0,0 +1,30 @@
+package forExample;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
+public class P_정수내림차순으로_배치 {
+	 public long solution(long n) {
+	      long answer = 0;
+	        String strResult="";
+	        String strn = String.valueOf(n);
+	        List<String> slist = Arrays.asList(strn.split(""));
+	        List<Integer> ilist = new ArrayList<Integer>();
+	        for(String s : slist) {
+	            ilist.add(Integer.parseInt(s));
+	        }
+	        ilist.sort(new Comparator<Integer>() {
+	            @Override
+	            public int compare(Integer o1, Integer o2) {
+	                return o2-o1;
+	            }
+	        });
+	        for(Integer i : ilist) {
+	            strResult+=String.valueOf(i);
+	        }
+	        answer = Long.parseLong(strResult);
+	        return answer;
+	  }
+}
