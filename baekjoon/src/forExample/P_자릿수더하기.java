2ef66900f835ffb6c7d6f002ceec444e288e9d3e
kwonseeun <0115km@naver.com>
2023-07-20 21:38:10 +0900

    프로그래머스_자릿수더하기

---
+++ baekjoon/src/forExample/P_자릿수더하기.java
@@ -0,0 +1,15 @@
+package forExample;
+
+public class P_자릿수더하기 {
+	 public int solution(int n) {
+	        int answer = 0;
+	        String s = Integer.toString(n);
+	 
+	        for (int i = 0; i < s.length(); i++) {
+	            answer += Integer.parseInt(s.substring(i, i+1));
+	        }
+	 
+	        return answer;
+	        
+	    }
+}
