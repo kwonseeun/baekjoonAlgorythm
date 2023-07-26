f66fe380f21d87d2a787e34f9f6f30e473411922
kwonseeun <0115km@naver.com>
2023-07-26 16:37:22 +0900

    b_10818

---
+++ baekjoon/src/forExample/b10818.java
@@ -0,0 +1,18 @@
+package forExample;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class b10818 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int arr[] = new int[n];
+		for (int i = 0; i < arr.length; i++) {
+			arr[i] = sc.nextInt();
+		}
+		Arrays.sort(arr);
+		System.out.println(arr[0]);
+		System.out.println(arr[arr.length-1]);
+	}
+}
