d5aae1c1ef95dad2023ca1b18a91c8f787b948a2
kwonseeun <0115km@naver.com>
2024-08-11 07:48:22 -0700

    B_단어길이재기

---
+++ baekjoon/src/baekjoon_june/B_단어길이재기.java
@@ -0,0 +1,18 @@
+package baekjoon_june;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class B_단어길이재기 {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		String st = br.readLine();
+		br.close();
+				
+		System.out.println(st.length());
+		
+		
+	}
+}
