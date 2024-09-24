0c80da2d18fcbc36322b344c43169f2bbdd23971
kwonseeun <0115km@naver.com>
2024-09-24 07:01:03 -0700

    B_돌게임

---
+++ baekjoon/src/baekjoon/B_돌게임.java
@@ -0,0 +1,19 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class B_돌게임 {
+	public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        if (n % 2 != 0) {
+            System.out.println("SK");
+        } else {
+            System.out.println("CY");
+        }
+    }
+}
