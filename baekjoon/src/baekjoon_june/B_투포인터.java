36a15f07e58c4dd7b7599ecd32345b736956cd42
kwonseeun <0115km@naver.com>
2024-09-24 07:02:02 -0700

    B_투포인터 수정

--- baekjoon/src/baekjoon_june/B_투포인터.java
+++ baekjoon/src/baekjoon_june/B_투포인터.java
@@ -1,9 +1,5 @@
 package baekjoon_june;
 
-import java.util.Scanner;
-
-import baekjoon09.B_어린왕자.Main;
-
 public class B_투포인터 {
 	 public int solution(int n){
 
@@ -18,11 +14,4 @@ public class B_투포인터 {
 	        return answer;
 	    }
 
-	    public static void main(String[] args){
-	        Main T = new Main();
-	        Scanner kb = new Scanner(System.in);
-	        int n=kb.nextInt();
-
-	        System.out.print(T.solution(n));
-	    }
 }
